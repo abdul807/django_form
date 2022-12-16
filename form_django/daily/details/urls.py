0f6c15240a88fcdbee3d240583d0e73cf0ce7e07
Usman Abdul Matin <amatin832@gmail.com>
2022-12-16 15:52:51 +0000

    Add files via upload

--- form_django/daily/details/urls.py
+++ form_django/daily/details/urls.py
@@ -8,5 +8,7 @@ app_name = 'detail'
 
 urlpatterns = [ 
     path('',views.index,name = 'index'),
-    path('signup',views.signup,name='signup')
+    path('signup',views.signup,name='signup'),
+    path('addData',views.addData,name='addData'),
+    path('home',views.view_data,name='view_data')
 ]
--- form_django/daily/details/views.py
+++ form_django/daily/details/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Details
 # Create your views here.
 
 
@@ -10,10 +10,38 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return render(request,'public/index.html'
+    all_names = Details.objects.all()
+    print(all_names)
+    return render(request,'public/index.html',
         # "name" : name.capitalize()
+        {
+            'names': all_names
+        }
     )
 
 
 def signup(request):
+
     return render(request,'public/signup.html')
+
+
+def addData(request):
+    name = request.POST.get('fname')
+    email = request.POST.get('email')
+    password = request.POST.get('password')
+
+    detail = Details(name=name,email=email,password=password)
+    detail.save()
+
+    print(detail)
+    return render(request, 'public/home.html')
+
+
+
+
+def view_data(request):
+    all_names = Details.objects.all()
+    print(all_names)
+    return render(request,'public/home.html',{ 
+        'names':all_names
+        })
